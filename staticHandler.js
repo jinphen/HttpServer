df0fe3327a3fc3c56d0b9753e2adcbc7096fa26b
jinphen <jinphen@126.com>
2014-11-04 23:04:45 +0800

    delete staticHandle's filenameHandle function

--- staticHandler.js
+++ staticHandler.js
@@ -14,78 +14,11 @@ var mime = require('./mime');
  */
 var cache = {};
 
-function fileHandle(filename, req, res) {
-	fs.stat(filename, function(err, stats){
-		if (err) {
-			res.writeHead(404, { 'Content-Type': 'text/html' });
-			res.end('<h1>Not Found</h1>');
-		} else {
-			//目录
-			if (stats.isDirectory()) {
-				if (!filename.match(/\/$/)) filename += '/';
-				filename += 'index.html';
-				fileHandle(filename, req, res);
-			} else {
-                //根据If-Modified-Since判断文件是否改变
-                var modifyDate = req.headers['If-Modified-Since'.toLowerCase()];
-                if (modifyDate && (new Date(modifyDate)).getTime() >= stats.mtime.getTime()) {
-                    res.writeHead(304, { 
-                        'Content-Type': mime.lookup(filename), 
-                        'Content-Length': 0,
-                        'Last-Modified': stats.mtime.toUTCString()
-                    });
-                    res.end();
-                } else {
-    				var fileInfo = cache[filename];
-    				//未修改
-    				if (fileInfo && fileInfo.mtime === stats.mtime.getTime()) {
-    					res.writeHead(200, { 
-    						'Content-Type': fileInfo.mime, 
-    						'Content-Length': fileInfo.size,
-                            'Last-Modified': stats.mtime.toUTCString()
-    					});
-    					res.end(fileInfo.data);
-    				} else {
-    					fs.readFile(filename, function(err, data){
-    						if (err) {
-    							res.writeHead(500, { 'Content-Type': 'text/plain' });
-    							res.end('file ' + filename + ' not readable ' + err);
-    						} else {
-    							var mimeType = mime.lookup(filename);
-    							var size = stats.size;
-
-    							//缓存
-    							cache[filename] = {
-    								mtime: stats.mtime.getTime(),
-    								size: size,
-    								mime: mimeType,
-    								data: data
-    							}
-
-    							//发送数据
-    							res.writeHead(200, { 
-                                    'Content-Type': mimeType,
-                                    'Content-Length': size,
-                                    'Last-Modified': stats.mtime.toUTCString()
-    							});
-    							res.end(data);
-    						}
-    					})
-    				}
-                }
-			}
-		}
-	});
-}
-
 exports.handle = function(req, res) {
 	if (req.method !== 'GET'){
 		res.writeHead(404, { 'Content-Type': 'text/plain' });
 		res.end('invalid method ' + req.method);
 	} else {
-		// var filename = req.basicServer.module.options.docroot + req.basicServer.urlParsed.pathname;
-		// fileHandle(filename, req, res);
-
         new SendStream(req, res).send();
 	}
 }
